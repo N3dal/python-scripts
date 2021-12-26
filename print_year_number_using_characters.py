ab7b121062ea0ab1372002b722c902be8cc5f423
Ned-84 <nedalxzo@gmail.com>
2021-12-26 22:04:47 +0200

    create print_num function that used to print the number in terminal using chars.

--- print_year_number_using_characters.py
+++ print_year_number_using_characters.py
@@ -33,8 +33,131 @@ def clear():
 clear()
 
 
+# create the numbers string.
+numbers = [
+
+
+
+    """##########
+##      ##
+##      ##
+##      ##
+##      ##
+##      ##
+##########""",
+
+    """    ##     
+  ## #     
+##  ##     
+    ##     
+    ##     
+    ##     
+###########""",
+
+    """##########
+        ##
+        ##
+##########
+##        
+##        
+##########""",
+
+    """##########
+        ##
+        ##
+##########
+        ##
+        ##
+##########""",
+
+    """##      ##
+##      ##
+##      ##
+##########
+        ##
+        ##
+        ##""",
+
+    """##########
+##        
+##       
+##########
+        ##
+        ##
+##########""",
+
+    """##########
+##          
+##       
+##########
+##      ##
+##      ##
+##########""",
+
+    """##########
+       ##
+      ## 
+     ##  
+    ##    
+   ##    
+  ##""",
+
+    """##########
+##      ##
+##      ##
+##########
+##      ##
+##      ##
+##########""",
+
+    """##########
+##      ##
+##      ##
+##########
+        ##
+        ##
+##########"""
+
+
+
+]
+
+
+def print_num(number: int = 2022, char: str = '#'):
+    """this function will print the number that you enter,
+    to ascii characters on the terminal."""
+
+    # guard condition.
+    if not isinstance(number, int):
+        # if the input was non-integer value.
+        return -1
+
+    # make sure that we use char only.
+    # so if the input (char-variable) was string we will take only one,
+    # char and its the first item.
+    char = char[0]
+
+    # note that any number as string is only seven lines.
+    final_number = ""
+    for line in range(7):
+        # print(f"XXXXXXXXXXXXXXXXXXXXX(line number {line+1}")
+        for num in str(number):
+            final_number += numbers[int(num)
+                                    ].split('\n')[line] + (' '*3)
+            # print(final_number)
+            # print()
+            # print()
+        final_number += '\n'
+
+    # now use the replace method to change the character from '#' to the,
+    # char var.
+    return final_number.replace('#', char)
+
+
 def main():
-    pass
+    # for num in numbers:
+    #     print(num)
+    print(print_num(2022, '$$'))
 
 
 if __name__ == "__main__":
