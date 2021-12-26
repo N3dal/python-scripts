f43aa20c3d20e239b8489338c3815c8f19b66b22
Ned-84 <nedalxzo@gmail.com>
2021-12-26 21:01:16 +0200

    create the print_year_number_using_character script, and setup the program main function.

---
+++ print_year_number_using_characters.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------------
+# this script will print the year number or any number in terminal,
+# using the chan that you use example:
+#
+# ###########    ###########
+#          ##    ##       ##
+#          ##    ##       ##
+# ###########    ##       ##
+# ##             ##       ##
+# ##             ##       ##
+# ###########    ###########
+#
+#
+# Author:N84.
+#
+# Create Date:Sun Dec 26 20:53:16 2021.
+# ///
+# ///
+# ///
+# -----------------------------------------------------------------
+
+
+from os import system
+
+
+def clear():
+    """function to wipe the terminal screen."""
+    system("clear")
+
+
+# wipe the terminal.
+clear()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
